29e82b65b0cd97db596d44da23a7d9d006fa00aa
testtaro <test@hoge.com>
2019-01-07 01:30:03 +0900

    各ページのタイトル変更、heroku設定、不要ルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-  get 'entry/index'
-  resources :counts
+  # resources :counts
   resources :comments
   resources :images
   resources :prices
   resources :hotels
-  resources :airports
-  resources :stations
-  resources :cities
-  resources :prefectures
-  resources :stay_units
-  resources :rent_units
-  resources :infomations
-  get 'admin/index'
+  # resources :airports
+  # resources :stations
+  # resources :cities
+  # resources :prefectures
+  # resources :stay_units
+  # resources :rent_units
+  # resources :infomations
+  # get 'admin/index'
   get 'welcome/index'
   get 'welcome/prefecture_search'
   get 'welcome/city_search'
   get 'welcome/hotel_search'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'entry/index'
+
   root 'welcome#index'
 
   post '/hotels/:id', to: 'comments#create'
